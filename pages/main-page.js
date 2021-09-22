1f726698f0ec6de53edef252e0d3835f3c5e97dd
IHprogram <ihm20202020@gmail.com>
2021-09-22 17:44:10 +0900

    楽天ブックス書籍検索APIから書籍情報を取得する関数を実装

--- pages/main-page.js
+++ pages/main-page.js
@@ -1,16 +1,28 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
 import Header from "../components/Header";
+import { searchBooks } from "../lib/books";
 
-const Mainpage = () => {
+const Mainpage = ({ searchResult }) => {
+
   return (
     <>
       <Header />
       <Layout title="Main Page">
-        <h2>メインページ</h2>
+        <button type="button" onClick={() => console.log(searchResult)}>確認</button>
       </Layout>
     </>
   )
 }
 
 export default Mainpage;
+
+export const getStaticProps = async () => {
+  const searchResult = await searchBooks('卓球');
+  console.log('searchResultの中身', searchResult);
+
+  return {
+    props: { searchResult },
+    revalidate: 3,
+  }
+}
